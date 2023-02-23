fb985b83e17da1f21561661177d1504dbb2639a6
xitan <xitan486@student.liu.se>
2023-02-23 23:25:50 +0100

    show todo list and can complete any of them

---
+++ src/views/TodosView.vue
@@ -0,0 +1,71 @@
+<script setup>
+import TodoCreater from "../components/TodoCreater.vue";
+import TodoItem from "../components/TodoItem.vue";
+import { Icon } from '@iconify/vue';
+import { uid } from 'uid';
+import { ref } from "vue";
+const todoList = ref([]);
+
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null,
+
+  });
+};
+
+const toggleTodoComplete = (todoPos) => {
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted
+};
+
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreater @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem 
+      v-for="(todo, index) in todoList" 
+      :todo="todo" 
+      :index="index" 
+      @toggle-complete="toggleTodoComplete"
+      />
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+      <span>You have no todo's to complete！Add one!</span>
+    </p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+  </style>
